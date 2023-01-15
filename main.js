a143f806e4dedcc4785f27d8cd1427fcb027cf9d
Matias Aristizabal Vargas <matiasaristizabalvargas9@gmail.com>
2023-01-15 15:49:57 -0500

    3 fusion de los archivos

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
-    console.log('Click');
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS Funcionando');
+function toggleMobileMenu() {  
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    } 
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive'); 
+    const ismobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isasideClosed = aside.classList.contains('inactive');
+    
+    if (!ismobileMenuClosed) {
+            mobileMenu.classList.add('inactive');  
+    } 
+    
+    
+}
